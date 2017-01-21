cc8465934fff00e4a8d70f6b8a2aeba1631b8065
Allison Chow <alchow@uwaterloo.ca>
2017-01-21 16:08:27 -0500

    stuffs

--- app/routes.js
+++ app/routes.js
@@ -5,44 +5,37 @@ var pyshell = new PythonShell('search.py'); // search.py opens two web browsers,
 
 module.exports = function(app) {
 
-  app.get('/', function(req, res) {
+  app.get('/', function(req, res) { // takes you to the initial page
     res.render('index.pug');
   });
 
-  app.get('/landing', function(req, res) {
+  app.get('/landing', function(req, res) { // takes you to the landing page after a match is/isn't found
     res.render('landing.pug');
   });
 
-  app.post('/search', function(req, res){
-    var app1 = new Clarifai.App(
+  app.post('/search', function(req, res){ // takes the url in the search field and tests it against the clarifai
+    var app1 = new Clarifai.App( // allows access to the clarifai app
       'ThQZUpvaf0LjZFmFpNku6LtN3zVEP92P6UYBmGCl',
       'hUSoLveXXpCZU-cMHZ2N2MGI9gIJERUgQojsLR3H'
     );
-    console.log(req.body);
-    var nameOfURL = req.body.URL;
+    // console.log(req.body);
+    var nameOfURL = req.body.URL; // takes the url from the form
 
-    app1.models.predict("TRUMP", [nameOfURL]).then(
+    app1.models.predict("TRUMP", [nameOfURL]).then( // calls the predict function for our model
       function(response) {
-        console.log("YAY");
-        console.log(response.status);
-        //var max = [];
-        var max = 0;
-        var name;
+        // console.log("YAY");
+        // console.log(response.status);
+        var max = 0; // variable to store the max data value by concept
+        var name; // stores the name of the person associated with the max data value
         //console.log(response[data]);
-        for (var i = 0; i < (response["outputs"][0]["data"]["concepts"]).length; i++) {
-          //max.push([response["outputs"][0]["data"]["concepts"][i]["name"],response["outputs"][0]["data"]["concepts"][i]["value"]]);
-          //console.log("Name: "+ response["outputs"][0]["data"]["concepts"][i]["name"]);
-          //console.log("Value: "+ response["outputs"][0]["data"]["concepts"][i]["value"]);
-          //console.log(max[i][0]);
-          //console.log(max[i][1]);
-          console.log(req.body);
+        for (var i = 0; i < (response["outputs"][0]["data"]["concepts"]).length; i++) { // runs through every concept
+          // console.log(req.body);
           if (response["outputs"][0]["data"]["concepts"][i]["value"] > max) {
             max = response["outputs"][0]["data"]["concepts"][i]["value"];
             name = response["outputs"][0]["data"]["concepts"][i]["name"];
           }
         }
         console.log(max);
-        //match.name = name;
         console.log(name);
         if (max < 0.5) {
           console.log("Error: No match");
@@ -50,7 +43,6 @@ module.exports = function(app) {
           console.log("OKAY");
           res.redirect('/landing');
           pyshell.send(name); // sends the name of the match to the python script
-
         }
       },
       function(err) {
