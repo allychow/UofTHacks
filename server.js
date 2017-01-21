0b8dd9bc0e5832279cc098a2fb0fad199ee1bae5
Susan Li <suzyn.one@gmail.com>
2017-01-21 02:52:39 -0500

    this is bad

---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+
+var server = http.createServer(function(req, res) {
+    var params = querystring.parse(url.parse(req.url).query);
+    res.writeHead(200, {"Content-type": "text/plain"});
+    if ('firstname' in params && 'lastname' in params) {
+        res.write('Your name is ' + params['firstname'] + ' ' + params['lastname']);
+    }
+    else {
+        res.write('Hello World! in text');
+    }
+    res.end();
+});
+
+const port = 7070;
+
+server.listen(port);
+console.log('Porting on ', {port});
