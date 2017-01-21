3deeaa70466e1a887a1053b2a9edbd8f81bc2486
Erick Leon <eri.lewo@hotmail.com>
2017-01-21 17:51:32 -0500

    fb integration

--- search.py
+++ search.py
@@ -5,3 +5,4 @@ line = sys.stdin.readline()
 
 webbrowser.open("https://google.ca/#q=" + line)
 webbrowser.open("http://internetthoughts.net/results?input=" + line)
+webbrowser.open("https://www.facebook.com/search/top/?q="+line)
