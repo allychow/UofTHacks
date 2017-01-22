df7c104de22eaff56de12fd6255891cdf9c64961
fliu98 <tianlunliu98@gmail.com>
2017-01-21 20:02:35 -0500

    Reverting to before line endings Revert "fb integration"
    This reverts commit 3deeaa70466e1a887a1053b2a9edbd8f81bc2486.

--- search.py
+++ search.py
@@ -5,4 +5,3 @@ line = sys.stdin.readline()
 
 webbrowser.open("https://google.ca/#q=" + line)
 webbrowser.open("http://internetthoughts.net/results?input=" + line)
-webbrowser.open("https://www.facebook.com/search/top/?q="+line)
