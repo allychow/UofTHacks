236f6562c23ee748fa7351a4fcd77d597e6aae30
fliu98 <tianlunliu98@gmail.com>
2017-01-21 21:41:40 -0500

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -61,3 +61,63 @@ module.exports = function(app) {
     );
   });
 }
+
+module.exports = function(app) {
+
+  app.get('/', function(req, res) { // takes you to the initial page
+    res.render('index.pug');
+  });
+
+  app.get('/landing', function(req, res) { // takes you to the landing page after a match is/isn't found
+    res.render('landing.pug');
+  });
+
+  //enum set of names:
+  //'donald_trump', 'hillary_clinton', 'barack_obama'
+  app.get('/landing/donald%20trump', function(req, res) {
+    res.render('index.pug');
+  });
+
+  app.post('/searchByBytes', function(req, res){
+    var app1 = new Clarifai.App( // allows access to our clarifai application
+      'ThQZUpvaf0LjZFmFpNku6LtN3zVEP92P6UYBmGCl',
+      'hUSoLveXXpCZU-cMHZ2N2MGI9gIJERUgQojsLR3H'
+    );
+    // console.log(req.body);
+    var imageBytes = req.body.imageBytes;
+    alert(imageBytes) // takes the url from the form
+
+    app1.models.predict("TRUMP", imageBytes).then( // calls the predict function for our model
+      function(response) {
+        // console.log("YAY");
+        // console.log(response.status);
+        var max = 0; // variable to store the max data value by concept
+        var name; // stores the name of the person associated with the max data value
+        //console.log(response[data]);
+        for (var i = 0; i < (response["outputs"][0]["data"]["concepts"]).length; i++) { // runs through every concept
+          // console.log(req.body);
+          if (response["outputs"][0]["data"]["concepts"][i]["value"] > max) { // finds the greatest comparison value
+            max = response["outputs"][0]["data"]["concepts"][i]["value"];
+            name = response["outputs"][0]["data"]["concepts"][i]["name"];
+          }
+        }
+        // console.log(max);
+        // console.log(name);
+        if (max < 0.5) { // must be greater than a match of 0.5 to be considered a match
+          console.log("Error: No match");
+          name = null;
+        } else {
+          // console.log("OKAY");
+          // res.redirect('/landing');
+          pyshell.send(name); // sends the name of the match to the python script
+
+          //name.replace(' ', '_'); // removes the whitespace in our concept names
+        }
+        res.redirect('/landing/' + name); // redirects to the landing page no matter what
+      },
+      function(err) { // error handling
+
+      }
+    );
+  });
+}
